1ec63666c92011ea49705742b0cc38a7f49f7b0b
Дмитро Затилюк <ipz223_zdo@student.ztu.edu.ua>
2024-02-13 11:30:35 +0200

    Додано .gitignore та створено консольний додаток

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введіть перше число:");
+string input1 = Console.ReadLine();
+
+Console.WriteLine("Введіть друге число:");
+string input2 = Console.ReadLine();
+
+// Конвертуємо введені значення у числа
+if (double.TryParse(input1, out double number1) && double.TryParse(input2, out double number2))
+{
+    // Обчислюємо суму
+    double sum = number1 + number2;
+
+    // Виводимо результат
+    Console.WriteLine($"Сума {number1} і {number2} дорівнює {sum}");
+}
+else
+{
+    Console.WriteLine("Неправильний формат введених даних. Будь ласка, введіть числа.");
+}
